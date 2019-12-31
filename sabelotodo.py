acbf0a0af0d43fe7fd04ccce1872b05c982dd3f0
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2019-12-31 17:21:06 +0100

    Update sabelotodo.py

--- sabelotodo.py
+++ sabelotodo.py
@@ -6,11 +6,11 @@ wikipedia.set_lang('es')
 speak=wc.Dispatch("Sapi.SpVoice")
 
 def habla(t):
-    #try:
-    summ = wikipedia.summary(t,sentences = 2)
-    speak.Speak(summ)
-    #except:
-        #print("Va a ser que no...")
+    try:
+        summ = wikipedia.summary(t,sentences = 2)
+        speak.Speak(summ)
+    except:
+        print("Va a ser que no...")
         
 
 while True:
