716a6e86ffcb5875d497d3bda683efd151602b36
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2019-12-31 19:16:34 +0100

    Update sabelotodo.py

--- sabelotodo.py
+++ sabelotodo.py
@@ -1,5 +1,6 @@
 import wikipedia
 import win32com.client as wc
+from gtts import gTTS
 from VALID import ns
 
 wikipedia.set_lang('es')
@@ -7,8 +8,12 @@ speak=wc.Dispatch("Sapi.SpVoice")
 
 def habla(t):
     try:
+        #REPRODUCE AUDIO
         summ = wikipedia.summary(t,sentences = 2)
         speak.Speak(summ)
+        #GUARDA AUDIO
+        tts = gTTS(summ, lang='es')
+        tts.save(t+".mp3")
     except:
         print("Va a ser que no...")
         
