c09d721dd8d9e658b09e19b9bb9fe3f76c061d0c
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2020-01-04 17:46:35 +0100

    Update sabelotodo.py

--- sabelotodo.py
+++ sabelotodo.py
@@ -12,14 +12,19 @@ def habla(t):
         summ = wikipedia.summary(t,sentences = 2)
         speak.Speak(summ)
         #GUARDA AUDIO
-        tts = gTTS(summ, lang='es')
-        tts.save(t+".mp3")
+        aud = ns(input("¿Descarga audio?: ")).lower()
+        if aud == "s":
+            nom = t+".mp3"
+            print("Generando archivo", nom)
+            tts = gTTS(summ, lang='es')
+            tts.save(nom)
     except:
         print("Va a ser que no...")
         
 
 while True:
     tema = input("Introduce tema: ")
+    print("RELACIONADOS: ",wikipedia.search(tema))
     habla(tema)
     conti = ns(input("¿Continuar?: "))
     if conti == "n":
