5cdc6812301988da45cb45715c376072ed3c03e9
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2020-01-10 18:58:11 +0100

    Update sabelotodo.py

--- sabelotodo.py
+++ sabelotodo.py
@@ -8,25 +8,29 @@ wikipedia.set_lang('es')
 speak=wc.Dispatch("Sapi.SpVoice")
 
 def habla(t):
-    try:
-        #REPRODUCE AUDIO
-        pagina = wikipedia.page(t)
-        summ = pagina.summary
-        print("\n"+summ+"\n")
-        text = re.sub("\[\d+\]","",summ)
-        speak.Speak(text)
-        #GUARDA AUDIO
-        aud = ns(input("¿Descarga audio?: ")).lower()
-        if aud == "s":
-            direc()
-            nom = t+".mp3"
-            print("Generando archivo", nom)
-            tts = gTTS(text, lang='es')
-            tts.save(nom)
-            print("Generado archivo", nom)
-    except:
-        print("NO SE PUDO COMPLETAR LA ACCIÓN")
-    print("\nARTÍCULOS RELACIONADOS: ",wikipedia.search(tema))
+    if t!="":
+        try:
+            #REPRODUCE AUDIO
+            if t!="":
+                pagina = wikipedia.page(t)
+                summ = pagina.summary
+                print("\n"+summ+"\n")
+                text = re.sub("\[\d+\]"," ",summ)
+                speak.Speak(text)
+                #GUARDA AUDIO
+                aud = ns(input("¿Descarga audio?: ")).lower()
+                if aud == "s":
+                    direc()
+                    nom = t+".mp3"
+                    print("Generando archivo", nom)
+                    tts = gTTS(text, lang='es')
+                    tts.save(nom)
+                    print("Generado archivo", nom)
+        except:
+            print("NO SE PUDO COMPLETAR LA ACCIÓN")
+        print("\nARTÍCULOS RELACIONADOS: ",wikipedia.search(tema))
+    else:
+        print("INTRODUZCA TEMA DE BÚSQUEDA")
         
 while True:
     tema = input("Introduce tema: ")
