52e061cd33d3fceb0294b4d502201999a6b5feb9
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2020-01-24 17:52:52 +0100

    Create sabelotodoII.py

---
+++ sabelotodoII.py
@@ -0,0 +1,60 @@
+import wikipedia
+from gtts import gTTS
+import re
+from VALID import ns, direc, OKI
+
+idioma = input("Seleccione idioma: ")
+wikipedia.set_lang(idioma)
+
+
+def crea_audio(ti,te):
+    direc()
+    nom = ti+".mp3"
+    print("Generando archivo", nom)
+    tts = gTTS(te, lang=idioma)
+    tts.save(nom)
+    print("Generado archivo", nom)
+
+def desamb(tem):
+    posibles_temas = wikipedia.search(tem)
+    if len(posibles_temas)>1:
+        print("********DESAMBIGUACIÓN********")
+        print(tem,"puede referirse a:")
+        for i,posible_tema in enumerate(posibles_temas):
+            print(i,posible_tema)
+        eleccion = OKI(input("Introduzca número correspondiente a su opción: "))
+        if eleccion <= (len(posibles_temas)-1):
+            assert eleccion in range(len(posibles_temas))
+            habla(posibles_temas[eleccion])
+        else:
+            print("VALOR DE ENTRADA INCORRECTO")
+    
+def habla(t):
+    if t!="":
+        try:
+            #REPRODUCE AUDIO
+            if t!="":
+                print("ACCEDIENDO...")
+                pagina = wikipedia.page(t)
+                summ = pagina.summary
+                print("\n"+summ+"\n")
+                #GUARDA AUDIO
+                aud = ns(input("¿Descargar un audio?: ")).lower()
+                if aud == "s":
+                    text = re.sub("\[\d+\]","",summ)
+                    #text = re.sub("km²","kilometros cuadrados",text)\[cita requerida\]
+                    text = re.sub("\[cita requerida\]","",text)
+                    crea_audio(t,text)
+                print("\nARTÍCULOS RELACIONADOS: ",wikipedia.search(tema))
+        except:
+            print("NO SE PUDO COMPLETAR LA ACCIÓN")
+            #ERROR DE DESAMBIGUACION
+            desamb(t)
+    else:
+        print("INTRODUZCA TEMA DE BÚSQUEDA")
+        
+while True:
+    tema = input("Introduce tema: ")
+    if tema == ".":
+        break
+    habla(tema)
